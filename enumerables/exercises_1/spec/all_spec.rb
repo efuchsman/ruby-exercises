2120ea03c810e007104ce95990ca81ca11a97072
Eli Fuchsman <efuchsman@gmail.com>
2022-09-05 18:25:58 -0600

    Complete all_spec

--- enumerables/exercises_1/spec/all_spec.rb
+++ enumerables/exercises_1/spec/all_spec.rb
@@ -7,65 +7,90 @@ RSpec.describe 'all test' do
     expect(all_zeros).to eq(true)
   end
 
-  xit 'not all zeroes' do
+  it 'not all zeroes' do
     numbers = [0, 0, 0, 0, 1, 0, 0, 0]
     all_zeros = numbers.all? do |number|
-      # Your code goes here
+      number.zero?
     end
     expect(all_zeros).to eq(false)
   end
 
-  xit 'all gone' do
-    words = ["gone", "gone", "gone", "gone", "gone", "gone", "gone"]
-    # Your code goes here
+  it 'all gone' do
+    words = %w[gone gone gone gone gone gone gone]
+    all_gone = words.all? do |word|
+      word.include?('gone')
+    end
     expect(all_gone).to eq(true)
   end
 
-  xit 'not all gone' do
-    words = ["gone", "gone", "gone", "gone", "gone", "there", "gone", "gone"]
+  it 'not all gone' do
+    words = %w[gone gone gone gone gone there gone gone]
     # Your code goes here
+    all_gone = words.all? do |word|
+      word.include?('gone')
+    end
     expect(all_gone).to eq(false)
   end
 
-  xit 'all empty' do
-    strings = ["", "", "", "", "", "", ""]
+  it 'all empty' do
+    strings = ['', '', '', '', '', '', '']
+    all_empty = strings.all? do |string|
+      string.empty?
+    end
     # Your code goes here
     expect(all_empty).to eq(true)
   end
 
-  xit 'not all empty' do
-    strings = ["", "", "", "full", "", "", ""]
+  it 'not all empty' do
+    strings = ['', '', '', 'full', '', '', '']
+    all_empty = strings.all? do |string|
+      string.empty?
+    end
     # Your code goes here
     expect(all_empty).to eq(false)
   end
 
-  xit 'not all uppercase' do
-    words = ["DOUGHNUT", "CASH", "MAIN", "bOWl", "SMACK", "SAND"]
+  it 'not all uppercase' do
+    words = %w[DOUGHNUT CASH MAIN bOWl SMACK SAND]
+    all_uppercase = words.all? do |word|
+      !word.upcase
+    end
     # Your code goes here
     expect(all_uppercase).to eq(false)
   end
 
-  xit 'all lies' do
+  it 'all lies' do
     lies = [false, false, false, false]
+    all_lies = lies.all? do |_lie|
+      _lie.equal?(false)
+    end
     # Your code goes here
     expect(all_lies).to eq(true)
   end
 
-  xit 'multiples of 7' do
+  it 'multiples of 7' do
     numbers = [42, 14, 35, 49, 28, 56, 21, 7]
-    # Your code goes here
+    all_multiples_of_7 = numbers.all? do |number|
+      number % 7 == 0
+    end
     expect(all_multiples_of_7).to eq(true)
   end
 
-  xit 'not all three digits long' do
+  it 'not all three digits long' do
     numbers = [981, 831, 509, 332, 892, 8999, 110]
     # Your code goes here
+    all_3_digits = numbers.all? do |number|
+      digi = number.digits.count
+      digi == 3
+    end
     expect(all_3_digits).to eq(false)
   end
 
-  xit 'all four letter words' do
-    words = ["love", "hate", "fire", "bird", "call"]
-    # Your code goes here
+  it 'all four letter words' do
+    words = %w[love hate fire bird call]
+    all_4_letters = words.all? do |word|
+      word.length == 4
+    end
     expect(all_4_letters).to eq(true)
   end
 end
