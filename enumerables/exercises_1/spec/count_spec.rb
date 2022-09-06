a1c2d943aceb806f99f1346f70bffa476e852d7f
Eli Fuchsman <efuchsman@gmail.com>
2022-09-05 20:25:22 -0600

    WIP count_spec

--- enumerables/exercises_1/spec/count_spec.rb
+++ enumerables/exercises_1/spec/count_spec.rb
@@ -1,30 +1,35 @@
-RSpec.describe 'count test' do
+# frozen_string_literal: true
 
+RSpec.describe 'count test' do
   it 'words with e' do
-    words = ["thing", "phone", "bark", "belt", "shoe", "bath"]
+    words = %w[thing phone bark belt shoe bath]
     tally = words.count do |word|
       word.include?('e')
     end
     expect(tally).to eq(3)
   end
 
-  xit 'numbers greater than 17' do
+  it 'numbers greater than 17' do
     numbers = [9, 18, 12, 17, 1, 3, 99]
     tally = numbers.count do |number|
-      # Your code goes here
+      number > 17
     end
     expect(tally).to eq(2)
   end
 
-  xit 'words that are uppercase' do
-    words = ["trousers", "SOCKS", "sweater", "Cap", "SHOE", "TIE"]
-    # Your code goes here
+  it 'words that are uppercase' do
+    words = %w[trousers SOCKS sweater Cap SHOE TIE]
+    tally = words.count do |word|
+      word == word.upcase
+    end
     expect(tally).to eq(3)
   end
 
-  xit 'words ending in ing' do
-    words = ["thought", "brake", "shin", "juice", "trash"]
-    # Your code goes here
+  it 'words ending in ing' do
+    words = %w[thought brake shin juice trash]
+    tally = words.count do |word|
+      word.chars.last(3) == %w[i n g]
+    end
     expect(tally).to eq(0)
   end
 
@@ -47,9 +52,8 @@ RSpec.describe 'count test' do
   end
 
   xit 'four letter words' do
-    words = ["bake", "bark", "corn", "apple", "wart", "bird", "umbrella", "fart"]
+    words = %w[bake bark corn apple wart bird umbrella fart]
     # Your code goes here
     expect(tally).to eq(6)
   end
 end
-
